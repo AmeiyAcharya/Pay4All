5a1729da4656fa9a7e633ab58f1b3e56e0575676
AmeiyAcharya <90678802+AmeiyAcharya@users.noreply.github.com>
2023-03-02 22:40:01 +0530

    First Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+async function main() {
+  
+  // We get the contract to deploy
+  const Cave = await hre.ethers.getContractFactory("dClap");
+  // We set the constructor of the contract within a message
+  const caveContract = await Cave.deploy();
+  await caveContract.deployed();
+  console.log("Cave deployed to:", caveContract.address);
+  //console.log(process.env.ETHERSCAN_API_KEY);
+
+  // console.log("verifying...");
+  // await verify(caveContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
